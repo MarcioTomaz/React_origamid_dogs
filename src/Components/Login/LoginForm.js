35c36e78cd0fffe3523c1105147b30b01f445cc4
MarcioTomaz <marciotmaz@gmail.com>
2022-09-15 15:39:37 -0300

    Form component

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginForm = () => {
 
@@ -28,19 +30,10 @@ const LoginForm = () => {
       <h1>Login</h1>
       <form action="" onSubmit={handleLogin}>
 
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
 
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-
-        <button>Entrar</button>
+        <Button >Entrar</Button>
       </form>
 
       <Link to="/login/criar" >Cadastro</Link>
