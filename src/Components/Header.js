255c529871ce3c636d352b2e16b1fafeee60ccab
MarcioTomaz <marciotmaz@gmail.com>
2022-09-15 17:33:01 -0300

    Validate Token

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
 import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
@@ -11,8 +14,11 @@ const Header = () => {
         <Link to={"/"} aria-label="Dogs - Home" className={styles.logo}>
           <Dogs />
         </Link>
+        {data ? <Link to={"/conta"} className={styles.login}> {data.nome} </Link>
+          : (
+            <Link to={"/login"} className={styles.login}> Login / Criar </Link>
+          )}
 
-        <Link to={"/login"} className={styles.login}>Login / Criar </Link>
 
       </nav>
     </header>
